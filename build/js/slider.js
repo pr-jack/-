0e48fbab72cdab8290d9b7898c59bd61761e3acf
pr-jack <prevgenij@gmail.com>
2019-12-28 22:06:46 +0200

    28.12.19

---
+++ build/js/slider.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".swiper-container",{cssMode:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination"},mousewheel:!0,keyboard:!0});
